2fab35e2dafa278cf5f82fea56b34f6b7270803c
Lahiru-Madhuwantha <lahirumadhuwantha293@gmail.com>
2023-09-29 23:16:17 +0530

    Item bar create

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -23,13 +23,77 @@ function Home() {
   return(
     <Container>
       <Navbar/>
-      
+      <Banner1>
+              <Banner1Text>
+              “Unleash her inner charm with our latest sunglass collection
+              tailored exclusively for girls. From vibrant shades to adorable
+              designs, these sunglasses are the ultimate style companion for
+              every little fashionista”
+
+                <Banner1Button>
+                  SHOP NOW
+                </Banner1Button> 
+              </Banner1Text>
+              <BannerLogo>
+                <img src="./Banner1.png" alt=""/>
+              </BannerLogo>           
+        </Banner1>
     </Container>
   );
 }
 
 const Container = styled.div`
 width: 100%;
+height: 100vh;
+display: flex;
+flex-direction: column;
+padding: 0px;
+`;
+
+//Banner 1
+const Banner1 = styled.div`
+width: 100%;
+height: 270px;
+background: linear-gradient(to right, White,#E91E63);
+display: flex;
+align-items: center;
+position: relative;
+margin-top: 0px;
+`;
+const Banner1Text = styled.div`
+font-size: 28px;
+font-weight: 600;
+color: #607D8B; 
+font-family: Judson;
+width: 100%;
+word-wrap: break-word;
+word-break: normal;
+margin: 50px 150px 90px 200px;
+
+alignment: center;
+`;
+
+const Banner1Button = styled.button`
+width: 15%;
+height: 35px;
+display: flex;
+align-items: center;
+justify-content: center;
+background-color: red;
+border: none;
+otline: none;
+border-radius: 12px;
+color: white;
+font-size: 15px;
+
+`;
+const BannerLogo = styled.div`
+margin-right: 0px;
+crusor: pointer;
+  img {
+    width: 411px;
+    margin-Button: 0px;
+  }
 `;
 
 export default Home;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 function Navbar() {
 
     return (
+      <ContainerMain>
         <Container>
           <Inner>
             <Logo>
@@ -22,11 +23,47 @@ function Navbar() {
             <RightContainer></RightContainer>
           </Inner>
         </Container>
+        <ItemBar>
+            <ItemBarButton>
+              <p>Eye Wear</p>
+            </ItemBarButton>
+            <ItemBarButton>
+              <p>Earrings</p>
+            </ItemBarButton>
+            <ItemBarButton>
+              <p>Neckless</p>
+            </ItemBarButton>
+            <ItemBarButton>
+              <p>Bracelets</p>
+            </ItemBarButton>
+            <ItemBarButton>
+              <p>Belts</p>
+            </ItemBarButton>
+            <BasketButton>
+              <h3>Cart Has</h3>
+              <p>0</p>
+              <h3>Items</h3>
+              <img src="./Cart.png" alt=""/>
+            </BasketButton>
+        </ItemBar>
+      </ContainerMain>
     );
 
     
 }
 
+//Main Page
+const ContainerMain = styled.div`
+  width: 100%;
+  min-width: 400px;
+  height: 120px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0px;
+  `;
+
+//Top Box
 const Container = styled.div`
 width: 100%;
 height: 60px;
@@ -35,6 +72,7 @@ display: flex;
 align-items: center;
 position: relative;
 `;
+
 const Inner = styled.div`
 width: 100%;
 display: flex;
@@ -63,7 +101,10 @@ input{
   border-radius: 5px 0px 0px 5px;
 }
 `;
-const RightContainer = styled.div``;
+const RightContainer = styled.div`
+
+`;
+
 const SearchIcon  = styled.div`
 background-color: red;
 height:100%;
@@ -82,13 +123,12 @@ width: 5%;
 height: 35px;
 background-color: red;
 border: none;
-otline: none;
+outline: none;
 border-radius: 12px;
 margin: 0px 200px 0px 0px;
-
+color: white;
 
 `;
-
 const Logo1 = styled.div`
 margin-left: 50px;
 crusor: pointer;
@@ -97,4 +137,56 @@ crusor: pointer;
     margin-top: 0px;
   }
 `;
+
+//Item BAr
+
+const ItemBar = styled.div`
+width: 100%;
+height: 60px;
+display: flex;
+align-items: center;
+`;
+
+const ItemBarButton = styled.div`
+padding: 5px;
+height: 80%;
+display: flex;
+align-items: center;
+flex-direction: row;
+justify-content: center;
+crussor: pointer;
+margin-left: 150px;
+`;
+
+const BasketButton = styled.button`
+display: flex;
+align-items: center;
+height: 80%;
+cursor: pointer;
+margin-left: 150px;
+border:none;
+outline: none;
+
+h3{
+  font-size: 15px;
+  align-items: center;
+  font-weight: 400;
+  margin-left: 10px;
+
+}
+
+img{
+  width: 80px;
+  margin-right: 10px;
+}
+p{
+  color: red;
+  font-weight: 500;
+  font-size:20px;
+  margin-left:10px;
+}
+`;
+
+
+
 export default Navbar;
